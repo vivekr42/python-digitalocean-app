8b54bcea666854e03bb8b8a2f5bc4e35e92c48ac
vivekr42 <vivekraj.bth@gmail.com>
2025-04-17 22:23:55 +0530

    updated complete app25

--- main.py
+++ main.py
@@ -153,4 +153,4 @@ app.router.add_get('/ws', ws_handler)
 app.router.add_static('/static/', path='./static', name='static')
 
 if __name__ == '__main__':
-    web.run_app(app, host="0.0.0.0", port=8080)
+    web.run_app(app, host="0.0.0.0", port=80)
