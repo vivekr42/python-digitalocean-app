e049afe8077de1dbee303b01b9f344db21bb192a
vivekr42 <vivekraj.bth@gmail.com>
2025-04-17 20:51:33 +0530

    updated app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "🚀 Hello from Flask App on DigitalOcean with CI/CD!"
+    return "🚀 Hello from Flask App on DigitalOcean with CI/CD!, this website is a demo created for farooq. here we have deployed python app usinging github actions."
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
