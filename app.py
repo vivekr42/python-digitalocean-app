9d2b8b8f31390ba5969ba3746e1d7dfaaa18f10c
vivekr42 <vivekraj.bth@gmail.com>
2025-04-17 18:00:11 +0530

    Initial commit

---
+++ app.py
@@ -0,0 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "🚀 Hello from Flask App on DigitalOcean with CI/CD!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
